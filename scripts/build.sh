ade4f093b12f55c2a27b9ac81ccaf569c2116077
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-03-21 19:01:29 +0100

    reconfiguring

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,24 @@
-# add, commit recent changes
 git add .
-git commit -m 'building'
+git commit -m 'clearing /docs'
 git checkout master
+# delete contents in /docs to avoid conflicts
+rm -rf ./docs
+# pull other changes
+git pull elewa master
 # enter gitbook source
-cd docs_src
+# cd docs_src
 # build new gitbook
 gitbook build
+# copy it out of the way for branch change
+# f to overwirte last publication
+cp -rf ./_book/. ./docs/ 
+# delete the /_book foler
+rm -rf _book
 # return to parent directory
-cd ..
+# cd ..
 # commit recent build
 git add .
 git commit -m 'building'
-
 
 
 # to execute this file run this command in terminal:
